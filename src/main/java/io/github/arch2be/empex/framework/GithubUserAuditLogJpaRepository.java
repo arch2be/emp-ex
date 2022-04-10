f5805353978a751548243f93684dcc276ced9aef
arch2be <jdvozef@gmail.com>
2022-04-10 18:54:26 +0200

    move JpaRepo interface into class

--- src/main/java/io/github/arch2be/empex/framework/GithubUserAuditLogJpaRepository.java
+++ src/main/java/io/github/arch2be/empex/framework/GithubUserAuditLogJpaRepository.java
@@ -1,10 +1,35 @@
 package io.github.arch2be.empex.framework;
 
+import io.github.arch2be.empex.application.ports.out.GithubUserAuditLogRepository;
+import io.github.arch2be.empex.domain.GithubUserAuditLog;
+import lombok.AllArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-interface GithubUserAuditLogJpaRepository extends JpaRepository<GithubUserAuditLogEntity, Long> {
+@Service
+@AllArgsConstructor
+class GithubUserAuditLogJpaRepository implements GithubUserAuditLogRepository {
+
+    private final GithubUserAuditLogRepo githubUserAuditLogRepository;
+
+    @Override
+    public Optional<GithubUserAuditLog> findGithubUserAuditLogByLogin(String login) {
+        return githubUserAuditLogRepository.findOneByLogin(login)
+                .map(entity -> GithubUserAuditLog.withId(entity.getId(), entity.getLogin(), entity.getRequestCount()));
+    }
+
+    @Override
+    public void save(GithubUserAuditLog toSave) {
+        githubUserAuditLogRepository.save(
+                new GithubUserAuditLogEntity(toSave.getId(), toSave.getLogin(), toSave.getRequestCount()));
+    }
+}
+
+@Repository
+interface GithubUserAuditLogRepo extends JpaRepository<GithubUserAuditLogEntity, Long> {
 
     Optional<GithubUserAuditLogEntity> findOneByLogin(String login);
 }
--- src/main/java/io/github/arch2be/empex/framework/GithubUserInfoResponse.java
+++ src/main/java/io/github/arch2be/empex/framework/GithubUserInfoResponse.java
@@ -1,14 +1,18 @@
 package io.github.arch2be.empex.framework;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 class GithubUserInfoResponse implements Serializable {
     String login;
     Integer id;
